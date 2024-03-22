6dfba7754ada1ea2e18c7614ad04cb75338c366a
manoj98gopal <manoj98gopal@gmail.com>
2024-03-22 14:54:20 +0530

    postgres db,dockeraised, django project setup completed

--- practiceProject/storefront/settings.py
+++ practiceProject/storefront/settings.py
@@ -73,10 +73,22 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'frotstore',  
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'frontstore-db',
+        'PORT': '5432',
     }
 }
 
