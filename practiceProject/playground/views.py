3ed4d78419c590836893f257479f60e48f928bef
manoj98gopal <manoj98gopal@gmail.com>
2024-03-25 21:46:54 +0530

    restframe work installed

--- practiceProject/playground/views.py
+++ practiceProject/playground/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from store.models import Product
 
 # Create your views here.
 
 def say_hello(request):
+
+    qs = Product.objects.all()[:5]
     
-    return render(request,'hello.html',{'name':'Manoj'})
+    
+    return render(request,'hello.html',{'qs':qs})
