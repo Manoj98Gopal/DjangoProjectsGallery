569c068599ad3e785ecfd29cec8cbc3ffb54a653
manoj98gopal <manoj98gopal@gmail.com>
2024-03-26 07:53:48 +0530

    custom field is created getting result too

--- practiceProject/store/serializer.py
+++ practiceProject/store/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-
+from .models import Product
+from decimal import Decimal
 
 
 
@@ -9,8 +10,16 @@ class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     slug = serializers.SlugField()
+    # here changing the field name 
     price = serializers.DecimalField(max_digits=6,decimal_places=2,source="unit_price")
- 
+    price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+    
+    
+    
+    def calculate_tax(self,product:Product):
+        
+        tax_price = Decimal(1.4)
+        return product.unit_price * tax_price
     
     
     
